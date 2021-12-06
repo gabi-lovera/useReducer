7ad24e454455d43d9020d1a9e546cad4607e4ac2
Lovera Gabriel <gabrielloveraa@gmail.com>
2021-12-06 14:17:17 -0300

    Initialize project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Contactos from "./components/Contactos";
+import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Contactos />
+    </>
+  );
+}
+
+export default App;
